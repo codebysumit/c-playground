4a3ec20dad85243b17276d84f465d4939f55040e
Sumit Das <87431704+codebysumit@users.noreply.github.com>
2025-10-15 15:51:17 +0530

    Update 05-Delete-From-Head.c

--- 14-Singly-Linked-List/05-Delete-From-Head.c
+++ 14-Singly-Linked-List/05-Delete-From-Head.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/* Define the Node structure properly */
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+/* Function declarations */
+Node* createNode(int data);
+void printLL(Node* head);
+void deleteAtHead(Node** head);
+
+/* Function to create a new node */
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+/* Function to print the linked list */
+void printLL(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+    free(current);
+}
+
+/* Function to delete a node at head */
+void deleteAtHead(Node** head) {
+    if(*head == NULL) {
+        printf("Linked List is Empty!\n");
+        return;
+    }
+    Node* temp = *head;
+    *head = head->next;
+    free(temp);
+}
+
+
+/* Main function */
+int main() {
+    Node* head = NULL;
+
+    Node* node1 = createNode(10);
+    Node* node2 = createNode(20);
+    Node* node3 = createNode(30);
+    Node* node4 = createNode(40);
+
+    head = node1;
+    head->next = node2;
+    head->next->next = node3;
+    head->next->next->next = node4;
+
+    printLL(head);
+
+    deleteAtHead(&head);
+    printLL(head);
+ 
+    return 0;
+}
